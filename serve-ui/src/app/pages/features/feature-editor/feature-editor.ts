bdfcbbaa2c800b3357cc9d237fe404c5189d89e7
Dmitriy Snitko <snitkody@gmail.com>
2026-07-23 10:36:21 +0300

    Обновить API редактирования YAML

---
+++ serve-ui/src/app/pages/features/feature-editor/feature-editor.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'feature-editor',
+  templateUrl: 'feature-editor.html',
+  styleUrl: 'feature-editor.scss',
+})
+export class FeatureEditor {}
