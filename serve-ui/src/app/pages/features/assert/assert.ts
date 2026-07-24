2cd12289474dc63bdad283dc12360d2ed7a7f6e1
Dmitriy Snitko <snitkody@gmail.com>
2026-07-24 09:45:43 +0300

    Добавить экспендеры для описаний assert

--- serve-ui/src/app/pages/features/assert/assert.ts
+++ serve-ui/src/app/pages/features/assert/assert.ts
@@ -1,6 +1,7 @@
 import { Component, effect, input, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { TuiIcon } from '@taiga-ui/core';
+import { TuiItem } from '@taiga-ui/cdk/directives/item';
+import { TuiExpand, TuiHint, TuiIcon } from '@taiga-ui/core';
 import { TuiBadge, TuiStatus } from '@taiga-ui/kit';
 import { Assertion } from '../../../model/assertion.model';
 import { Markdown } from '../../../ui/markdown/markdown.component';
@@ -9,7 +10,7 @@ import { Markdown } from '../../../ui/markdown/markdown.component';
   selector: 'assert',
   templateUrl: 'assert.html',
   styleUrl: 'assert.scss',
-  imports: [FormsModule, Markdown, TuiBadge, TuiStatus, TuiIcon],
+  imports: [FormsModule, Markdown, TuiBadge, TuiStatus, TuiIcon, TuiExpand, TuiItem, TuiHint],
 })
 export class Assert {
   assertion = input.required<Assertion>();
